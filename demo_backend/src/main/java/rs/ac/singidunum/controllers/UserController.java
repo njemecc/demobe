f51f2c9e53181d791130ddafca8f115a958dc0d9
brani <branislannjemec@gmail.com>
2023-05-14 23:19:03 +0200

    "validacija"

--- demo_backend/src/main/java/rs/ac/singidunum/controllers/UserController.java
+++ demo_backend/src/main/java/rs/ac/singidunum/controllers/UserController.java
@@ -1,6 +1,10 @@
 package rs.ac.singidunum.controllers;
 
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import rs.ac.singidunum.models.UserModel;
 import rs.ac.singidunum.services.IUserService;
@@ -23,28 +27,37 @@ public class UserController {
         return "Home page";
     }
 
-    @GetMapping("/getAllUsers")
-    @CrossOrigin()
+    @GetMapping("getAllUsers")
+    @CrossOrigin("*")
     public List<UserModel> getAll()
     {
         return iUserService.getAll();
     }
 
-    @PostMapping("/create")
+    @PostMapping("create")
 
-   public UserModel Create(String firstName,String lastName,String email)
+    public ResponseEntity<?>Create(@RequestBody @Valid UserModel model, BindingResult result)
     {
-
-
-       UserModel model = new UserModel();
-
-        model.setLastName(lastName);
-        model.setFirstName(firstName);
-        model.setEmail(email);
-
-
-
-
-        return iUserService.Create(model);
+        if (result.hasErrors())
+        {
+            return new ResponseEntity<>(result.getAllErrors(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(iUserService.Create(model),HttpStatus.OK);
     }
+
+//   public UserModel Create(String firstName,String lastName,String email)
+//    {
+//
+//
+//       UserModel model = new UserModel();
+//
+//        model.setLastName(lastName);
+//        model.setFirstName(firstName);
+//        model.setEmail(email);
+//
+//
+//
+//
+//        return iUserService.Create(model);
+//    }
 }
--- demo_backend/src/main/java/rs/ac/singidunum/models/UserModel.java
+++ demo_backend/src/main/java/rs/ac/singidunum/models/UserModel.java
@@ -3,36 +3,62 @@ package rs.ac.singidunum.models;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
 
 @Entity
 @Table(name="users")
+@Data
 
 public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NotBlank
     @Column(name = "first_name")
     private String firstName;
+
+    @NotBlank
     @Column(name = "last_name")
     private String lastName;
+
     @Column(name = "email")
+    @Email
+    @NotBlank
     private String email;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
 }
